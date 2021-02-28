fb5062b5687cce2899caf1322bc6ce7c94cec181
DoerriesT <tim.doerries@gmx.de>
2021-02-28 14:11:10 +0100

    - added eastl

--- VEngine2/src/main.cpp
+++ VEngine2/src/main.cpp
@@ -1,8 +1,26 @@
 #include "Engine.h"
+#include <EASTL/fixed_vector.h>
+
+void *__cdecl operator new[](size_t size, const char *name, int flags, unsigned debugFlags, const char *file, int line)
+{
+	return new uint8_t[size];
+}
+
+void *__cdecl operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char *pName, int flags, unsigned debugFlags, const char *file, int line)
+{
+	return new uint8_t[size];
+}
 
 
 int main(int argc, char *argv[])
 {
+	eastl::fixed_vector<int, 10> v;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		v.push_back(i);
+	}
+
 	Engine engine;
 	return engine.start(argc, argv);
 }
