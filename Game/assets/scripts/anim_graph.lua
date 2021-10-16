1f638d135cac95c47898c1f29ae96d66f728a608
DoerriesT <tim.doerries@gmx.de>
2021-10-16 19:26:36 +0200

    - added temporal smoothing to animation blend - switched render backend back to vulkan

--- Game/assets/scripts/anim_graph.lua
+++ Game/assets/scripts/anim_graph.lua
@@ -8,11 +8,15 @@ local AnimGraphScript =
     }
 }
 
+local speedHistory = 0.0
+
 function AnimGraphScript:update(graph, ecs, entity, deltaTime)
 
     local mc = ecs:getComponent(entity, ComponentIDs['CharacterMovementComponent'])
 	local speed = math.sqrt(mc.m_velocityX * mc.m_velocityX + mc.m_velocityZ * mc.m_velocityZ)
-	graph:setFloatParam('speed', speed)
+	local alpha = 0.15
+	speedHistory = speed * alpha + speedHistory * (1.0 - alpha)
+	graph:setFloatParam('speed', speedHistory)
 	
 end
 
